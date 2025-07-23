34974e30479838607ac0cf0a51040258d47b6c78
sourbh20010111 <sourbhsonkar24@gmail.com>
2025-07-23 19:48:18 +0530

    Initial commit

--- java/Fibonacci.java
+++ java/Fibonacci.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Fibonacci {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter iteration: ");
+        int iteration=sc.nextInt();
+        int a=0;
+        int b=1;
+        
+        for(int i=1;i<=iteration;i++){
+           System.out.print(a+" ");
+           int next=a+b;
+           a=b;
+           b=next;
+           
+        }
+
+        sc.close();
+    }
 }
