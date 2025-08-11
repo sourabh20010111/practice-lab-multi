157d8b6ff4c4bae8cf7c44124cab58963dc32a5b
sourbh20010111 <sourbhsonkar24@gmail.com>
2025-08-11 11:10:19 +0530

    selenium JavaScriptExecutor

---
+++ SoftwareTesting/Automation/Selenium/SeleniumPracticeHub/src/selenium9/miscellaneous/javascriptexecutor/JavaScriptExecutorExample.java
@@ -0,0 +1,36 @@
+package selenium9.miscellaneous.javascriptexecutor;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class JavaScriptExecutorExample {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        // Cast driver to JavascriptExecutor
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        // Example 1: Show alert popup using JS
+        js.executeScript("alert('Hello from JavaScriptExecutor!');");
+
+        // Wait a bit to see alert (optional)
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Accept alert
+        driver.switchTo().alert().accept();
+
+        // Example 2: Get page title via JS and print it
+        String title = (String) js.executeScript("return document.title;");
+        System.out.println("Page title is: " + title);
+
+        driver.quit();
+	}
+
+}
